3b31077c626842587e81857ddb641fbca7f07ae9
guysaint <guysaint@naver.com>
2025-11-04 15:03:48 +0900

    ros2 pkg create 명령으로 폴더 구조 다시 세팅중

--- src/wasd_ws/src/wasd_bringup/setup.py
+++ src/wasd_ws/src/wasd_bringup/setup.py
@@ -1,30 +1,29 @@
-from setuptools import setup
-import os
-from glob import glob
+from setuptools import find_packages, setup
 
 package_name = 'wasd_bringup'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='wasd',
-    maintainer_email='you@example.com',
-    description='WASD project bringup package for TurtleBot3',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer_email='guysaint@naver.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
-            # 실행할 파이썬 노드가 생기면 여기 추가
         ],
     },
 )
