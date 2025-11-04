e89d1e3e18a70154234d32babdd4ffa6d165689c
guysaint <guysaint@naver.com>
2025-11-04 16:23:15 +0900

    setup.py 수정

--- src/wasd_ws/src/wasd_bringup/setup.py
+++ src/wasd_ws/src/wasd_bringup/setup.py
@@ -1,29 +1,35 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'wasd_bringup'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
+        # 패키지 인덱스용 리소스
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
+        # package.xml 설치
         ('share/' + package_name, ['package.xml']),
+        # 🔽 launch/*.py 를 install/share/wasd_bringup/launch 로 설치
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.py')),
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='wasd',
-    maintainer_email='guysaint@naver.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer_email='you@example.com',
+    description='WASD project bringup package for TurtleBot3',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            # 지금은 실행할 노드 없음
         ],
     },
 )
