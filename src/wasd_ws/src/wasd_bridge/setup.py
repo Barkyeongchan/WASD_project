40ab150affff9e5940df2424fccfb3b356371086
guysaint <guysaint@naver.com>
2025-11-12 15:52:08 +0900

    setup.py 수정

--- src/wasd_ws/src/wasd_bridge/setup.py
+++ src/wasd_ws/src/wasd_bridge/setup.py
@@ -1,29 +1,33 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'wasd_bridge'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.1.0',
+    packages=[package_name],  # wasd_bridge/ 디렉토리를 파이썬 패키지로 설치
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        # ament index
+        (os.path.join('share', 'ament_index', 'resource_index', 'packages'),
+         ['resource/' + package_name]),
+        # package.xml
+        (os.path.join('share', package_name), ['package.xml']),
+        # config (points.yaml 설치)
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='wasd',
     maintainer_email='guysaint@naver.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='WASD bridge between UI and Nav2',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            # ros2 run wasd_bridge wasd_bridge
+            'wasd_bridge = wasd_bridge.wasd_bridge_node:main',
         ],
     },
 )
