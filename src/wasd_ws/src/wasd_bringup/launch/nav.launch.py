7e320ab6ac1310321d49a76c57914cd525e8bf43
guysaint <guysaint@naver.com>
2025-11-20 15:41:59 +0900

    터틀봇 설정 파일을 읽어 오지 못했던 오류 수정(nav.launch.py), 이에 따른 로봇 튜닝값 제대로 적용 시켜서 구동 확인함

--- src/wasd_ws/src/wasd_bringup/launch/nav.launch.py
+++ src/wasd_ws/src/wasd_bringup/launch/nav.launch.py
@@ -1,106 +1,92 @@
 #!/usr/bin/env python3
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Darby Lim
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-ROS_DISTRO = os.environ.get('ROS_DISTRO')
-
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    map_dir = LaunchConfiguration(
+
+    # 맵 절대 경로
+    map_file = '/home/wasd/WASD_project/src/wasd_ws/wasd_map.yaml'
+
+    # burger.yaml 절대 경로
+    params_file = '/home/wasd/WASD_project/src/wasd_ws/src/wasd_bringup/param/burger.yaml'
+
+    map_arg = LaunchConfiguration(
         'map',
-        default=os.path.join(
-            get_package_share_directory('wasd_bringup'),
-            'map',
-            'map.yaml'))
+        default=map_file
+    )
 
-    param_file_name = TURTLEBOT3_MODEL + '.yaml'
-    if ROS_DISTRO == 'humble':
-        param_dir = LaunchConfiguration(
-            'params_file',
-            default=os.path.join(
-                get_package_share_directory('turtlebot3_navigation2'),
-                'param',
-                ROS_DISTRO,
-                param_file_name))
-    else:
-        param_dir = LaunchConfiguration(
-            'params_file',
-            default=os.path.join(
-                get_package_share_directory('turtlebot3_navigation2'),
-                'param',
-                param_file_name))
+    params_arg = LaunchConfiguration(
+        'params_file',
+        default=params_file
+    )
 
-    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    # nav2_bringup 의 bringup_launch.py
+    nav2_launch_file = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'launch',
+        'bringup_launch.py'
+    )
 
-    rviz_config_dir = os.path.join(
+    # RViz 설정은 기존 TB3 설정 사용
+    rviz_config = os.path.join(
         get_package_share_directory('turtlebot3_navigation2'),
         'rviz',
-        'tb3_navigation2.rviz')
+        'tb3_navigation2.rviz'
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'map',
-            default_value=map_dir,
-            description='Full path to map file to load'),
+            default_value=map_arg,
+            description='Full path to map file to load'
+        ),
 
         DeclareLaunchArgument(
             'params_file',
-            default_value=param_dir,
-            description='Full path to param file to load'),
+            default_value=params_arg,
+            description='Full path to param file to load'
+        ),
 
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
-            launch_arguments={
-                'map': map_dir,
-                'use_sim_time': use_sim_time,
-                'params_file': param_dir}.items(),
+            description='Use simulation (Gazebo) clock if true'
         ),
 
+        # Nav2 bringup 
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_launch_file),
+            launch_arguments={
+                'map': map_arg,
+                'use_sim_time': use_sim_time,
+                'params_file': params_arg,
+            }.items(),
+        ),
+
+        # RViz 실행
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', rviz_config_dir],
+            arguments=['-d', rviz_config],
             parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
-    ])
-
-'''
-        Node(
-            package='wasd_bringup',
-            executable='wasd_goal_proxy',
-            name='wasd_goal_proxy',
-            output='screen',
-            emulate_tty=True,
+            output='screen'
         ),
-        '''
+
+        # (선택) wasd_goal_proxy 계속 쓸 거면 남겨두고, 안 쓰면 주석 처리
+        # Node(
+        #     package='wasd_bringup',
+        #     executable='wasd_goal_proxy',
+        #     name='wasd_goal_proxy',
+        #     output='screen',
+        #     emulate_tty=True
+        # ),
+    ])
